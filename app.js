a7ebfab5c5f353d5d7cbd12c397e59e30a1be8f3
Jonathan-Agreda <flavioagreda1990@gmail.com>
2025-03-29 21:17:43 -0500

    managment for Render

--- app.js
+++ app.js
@@ -17,5 +17,5 @@ app.use("/", indexRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en puerto:${PORT}`);
 });
